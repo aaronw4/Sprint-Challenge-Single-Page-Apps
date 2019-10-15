809a6f926263d51ed1d056f440709bc5cac59802
Aaron Williams <aaronrw4@yahoo.com>
2019-10-14 20:04:03 -0700

    copied harry potter search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,46 +1,78 @@
-import React from "react";
-import {withFormik, Form, Field} from 'formik';
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
+const characters = [
+  "Harry Potter",
+  "Luna Lovegood",
+  "Neville Longbottom",
+  "Hermione Granger",
+  "Ron Weasley",
+  "Ginny Weasley",
+  "Fred Weasley",
+  "George Weasley",
+  "Albus Dumbledore ",
+  "Aberforth Dumbledore ",
+  "Dudley Dursley ",
+  "Petunia Dursley ",
+  "Vernon Dursley",
+  "Cornelius Fudge",
+  "Rubeus Hagrid ",
+  "Viktor Krum ",
+  "Bellatrix Lestrange",
+  "Narcissa Malfoy",
+  "Draco Malfoy"
+];
+
 function SearchForm() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const results = characters.filter(character =>
+      
+      character.toLowerCase().includes(searchTerm)
+    
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+  // The handleChange method takes the event object as the arguement and sets the current value of the form to the searchTerm state using setSearchTerm
+  const handleChange = event => {
+    // console.log(event.target.value)
+    setSearchTerm(event.target.value);
+    console.log(searchTerm);
+  };
+
   return (
-    <section className="search-form">
-      <Form className='form'>
-        <Field type='text' name='search' className='input'></Field>
-        <button type='submit' className='submit'>Search</button>
-      </Form>
-    </section>
+    <div className="App">
+      <form>
+        <label for="name">Search:</label>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </form>
+      <div className="character-list">
+        <ul>
+          {searchResults.map(character => (
+            <li>{character}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
-const myMapPropsToValues = props => {
-  const returnObj = {
-    search: props.search || ''
-  };
-  return returnObj;
-}
 
-const myHandleSubmit = (values, {setStatus}) => {
- 
-  axios
+export default SearchForm
+  /*axios
     .get('https://rickandmortyapi.com/api/character/')
     .then(response => {
       let data = response.data.results;
       return data;      
     })
     .catch(err => console.log(err));
-  
-  //console.log(data);
-  //let name = arr.map(char => char.name);
-  //console.log(name)
-};
-
-const formikObj = {
-  mapPropsToValues: myMapPropsToValues,
-  handleSubmit: myHandleSubmit
-}
-
-const EnhancedForm = withFormik(formikObj)
-const EnhancedSearchForm = EnhancedForm(SearchForm);
-
-export default EnhancedSearchForm
+  */
